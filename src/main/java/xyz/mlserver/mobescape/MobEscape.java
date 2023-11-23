0ccbfdec1b92494d4f6626deb871ed2c48b30a3e
Monster2408 <meoto2408@gmail.com>
2023-11-23 11:34:30 +0900

    add file

---
+++ src/main/java/xyz/mlserver/mobescape/MobEscape.java
@@ -0,0 +1,17 @@
+package xyz.mlserver.mobescape;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MobEscape extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
