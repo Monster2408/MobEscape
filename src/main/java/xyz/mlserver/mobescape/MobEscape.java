0c35d7eda3ce9626b81390fbb36975e033debc9f
Monster2408 <meoto2408@gmail.com>
2023-12-21 00:14:29 +0900

    up

--- src/main/java/xyz/mlserver/mobescape/MobEscape.java
+++ src/main/java/xyz/mlserver/mobescape/MobEscape.java
@@ -8,6 +8,8 @@ public final class MobEscape extends JavaPlugin {
     public static CustomConfiguration config;
     public static CustomConfiguration messageYml;
 
+    private static JavaPlugin plugin;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
@@ -15,10 +17,16 @@ public final class MobEscape extends JavaPlugin {
         config.saveDefaultConfig();
         messageYml = new CustomConfiguration(this, "message.yml");
         messageYml.saveDefaultConfig();
+
+        plugin = this;
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static JavaPlugin getPlugin() {
+        return plugin;
     }
 }
--- src/main/java/xyz/mlserver/mobescape/utils/game/Arena.java
+++ src/main/java/xyz/mlserver/mobescape/utils/game/Arena.java
@@ -1,15 +1,25 @@
 package xyz.mlserver.mobescape.utils.game;
 
+import com.google.gson.Gson;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import xyz.mlserver.mobescape.MobEscape;
 import xyz.mlserver.mobescape.utils.bukkit.LocationParser;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Arena {
 
     private String owner;
     private String name;
     private final int id;
-    private int maxPlayer = -1;
-    private int minPlayer = -1;
+    private int maxPlayer;
+    private int minPlayer;
     private String spawn;
 
     public Arena(String owner, String name, int id, String spawn) {
@@ -64,6 +74,73 @@ public class Arena {
 
     public void setSpawn(Location spawn) {
         this.spawn = LocationParser.parseJson(spawn);
+    }
+
+    // ---------------------------- //
+
+    private static final File file = new File(MobEscape.getPlugin().getDataFolder() + "/arena");
+
+    public static boolean checkFile() {
+        if (!file.exists()) return file.mkdirs();
+        return true;
+    }
+
+    private static List<Arena> arenas;
+
+    public static List<Arena> getArenas(){
+        if (arenas == null) arenas = new ArrayList<>();
+        return arenas;
+    }
+
+    private static HashMap<Integer, Arena> arenaMap;
+
+    public static HashMap<Integer, Arena> getArenaMap(){
+        if (arenaMap == null) arenaMap = new HashMap<>();
+        return arenaMap;
+    }
+
+    private static HashMap<Player, Arena> editingArena;
+
+    public static HashMap<Player, Arena> getEditingArena(){
+        if (editingArena == null) editingArena = new HashMap<>();
+        return editingArena;
+    }
+
+    public static void createArena(Player player, String name){
+        int id = getNewId();
+        Arena arena = new Arena(player.getUniqueId().toString(), name, id, null);
+        getArenaMap().put(id, arena);
+        getEditingArena().put(player, arena);
+    }
+
+    public static void saveArena(){
+        if (getArenas().isEmpty()) return;
+        checkFile();
+        for (Arena arena : getArenas()) {
+            File file = new File(MobEscape.getPlugin().getDataFolder() + "/arena/" + arena.getId() + ".yml");
+            YamlConfiguration yaml = parseYaml(arena);
+            try {
+                yaml.save(file);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static YamlConfiguration parseYaml(Arena arena){
+        Gson gson = new Gson();
+        YamlConfiguration yaml = new YamlConfiguration();
+        yaml.set("data", gson.toJson(arena));
+        return yaml;
+    }
+
+    public static int getNewId() {
+        int id = 0;
+        checkFile();
+        while (true) {
+            if (!getArenaMap().containsKey(id)) return id;
+            id++;
+        }
     }
 
 }
